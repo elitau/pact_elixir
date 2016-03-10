7549ef459ffbc94d40f64bedbaffb40fc02f7dd7
Ronald Holshausen <uglyog@gmail.com>
2016-03-10 13:28:08 +1100

    added basic arg passing to the CLI

--- pact_mock_server/src/main.rs
+++ pact_mock_server/src/main.rs
@@ -1,3 +1,48 @@
+extern crate getopts;
+use getopts::Options;
+use std::env;
+
+static SPEC_VERSION: &'static str = "1.0.0";
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn setup_options() -> Options {
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "print this help menu");
+    opts.optflag("v", "version", "print the mock server version");
+    opts
+}
+
+fn print_version() {
+    const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+    println!("pact mock server version  : v{}", VERSION.unwrap_or("unknown"));
+    println!("pact specification version: v{}", SPEC_VERSION);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let opts = setup_options();
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => {
+            println!("ERROR: {}", f);
+            print_usage(&program, opts);
+            println!("");
+            panic!(f.to_string())
+        }
+    };
+    if args.len() == 1 || matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    if matches.opt_present("v") {
+        print_version();
+        return;
+    }
 }
