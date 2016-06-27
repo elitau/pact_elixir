a0024bdf450499de0fa8a04326832733f7877ed8
Ronald Holshausen <rholshausen@dius.com.au>
2016-06-27 20:34:17 +1000

    added a osx release script for libpact_mock_server

---
+++ rust/v1/libpact_mock_server/release-osx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+cargo clean
+cargo build --release
+cargo build --release --target x86_64-apple-ios
+gzip -c target/release/libpact_mock_server.dylib > target/release/libpact_mock_server-osx-xf86_64-$1.dylib.gz
+gzip -c target/release/libpact_mock_server.a > target/release/libpact_mock_server-osx-xf86_64-$1.a.gz
+cargo build --release --target x86_64-apple-ios
+gzip -c target/x86_64-apple-ios/release/libpact_mock_server.a > target/x86_64-apple-ios/release/libpact_mock_server-ios-x86_64-$1.a.gz
