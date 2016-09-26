d7c859ce4dcaed006722822ad2dbbce115ac8e20
Ronald Holshausen <uglyog@gmail.com>
2016-09-26 20:55:12 +1000

    bump version to 0.0.3

--- rust/pact_matching/release.groovy
+++ rust/pact_matching/release.groovy
@@ -98,23 +98,12 @@ ask('Publish library to crates.io?: [Y]') {
   executeOnShell 'cargo publish'
 }
 
-ask('Copy docs to pact foundation github pages project?: [Y]') {
-  def docDir = System.console().readLine('Copy the library docs to: [../../../../pact-foundation.github.io/reference/rust/]').trim()
-  if (docDir.empty) {
-    docDir = '../../../../pact-foundation.github.io/reference/rust/'
-  }
-
-  executeOnShell "mkdir -p $docDir/libpact_matching-docs-$releaseVer"
-  executeOnShell "cp -r target/doc/* $docDir/libpact_matching-docs-$releaseVer/"
-  executeOnShell "rm -r $docDir/libpact_matching-docs-latest/*"
-  executeOnShell "cp -r target/doc/* $docDir/libpact_matching-docs-latest/"
-}
 executeOnShell "tar cvfz libpact_matching-docs-${releaseVer}.tgz *", new File("./target/doc")
 
 def nextVer = Version.valueOf(releaseVer).incrementPatchVersion()
 ask("Bump version to $nextVer?: [Y]") {
   executeOnShell "sed -i -e 's/version = \"${releaseVer}\"/version = \"${nextVer}\"/' Cargo.toml"
-  executeOnShell "sed -i -e 's/documentation = \"http:\\/\\/www.pact.io\\/reference\\/rust\\/libpact_matching-docs-${releaseVer}\\/pact_matching\\/\"/documentation = \"http:\\/\\/www.pact.io\\/reference\\/rust\\/libpact_matching-docs-${nextVer}\\/pact_matching\\/\"/' Cargo.toml"
+  executeOnShell "sed -i -e 's/documentation = \"https:\\/\\/docs.rs\\/pact_matching\\/${releaseVer}\\/pact_matching/\"/documentation = \"https:\\/\\/docs.rs\\/pact_matching\\/${nextVer}\\/pact_matching\"/' Cargo.toml"
   executeOnShell("git add Cargo.toml")
   executeOnShell("git diff --cached")
   ask("Commit and push this change?: [Y]") {
