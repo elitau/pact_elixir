ff5717febf64011515349f82d921b62f006af251
Ronald Holshausen <uglyog@gmail.com>
2016-09-27 21:02:03 +1000

    prep release for the verifier CLI

---
+++ rust/pact_verifier_cli/CHANGELOG.md
@@ -0,0 +1,1 @@
+To generate the log, run `git log --pretty='* %h - %s (%an, %ad)' TAGNAME..HEAD .` replacing TAGNAME and HEAD as appropriate.
---
+++ rust/pact_verifier_cli/release-osx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -xe
+cargo clean
+cargo build --release
+cargo build --release --target x86_64-apple-ios
+gzip -c target/release/pact_verifier_cli > target/release/pact_verifier_cli-osx-x86_64-$1.gz
+cargo build --release --target x86_64-apple-ios
+gzip -c target/x86_64-apple-ios/release/pact_verifier_cli > target/x86_64-apple-ios/release/pact_verifier_cli-ios-x86_64-$1.gz
