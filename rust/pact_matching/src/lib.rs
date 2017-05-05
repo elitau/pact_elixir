f725ddcc7b627579ea1e291e3b01bf3ac79a5067
Anthony Damtsis <adamtsis@dius.com.au>
2017-05-05 15:39:27 +1000

    Remove commented crate

--- rust/pact_matching/src/lib.rs
+++ rust/pact_matching/src/lib.rs
@@ -327,8 +327,7 @@
 
 #![warn(missing_docs)]
 #[macro_use] extern crate serde_json;
-#[macro_use]extern crate serde_derive;
-//extern crate serde;
+#[macro_use] extern crate serde_derive;
 #[macro_use] extern crate log;
 #[macro_use] extern crate p_macro;
 #[macro_use] extern crate maplit;
