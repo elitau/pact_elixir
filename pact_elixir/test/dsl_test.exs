ac0c10aa7cb9e65774604165f8974fcf87a0cb51
Eduard Litau <eduard.litau@gmail.com>
2017-10-20 01:23:49 +0200

    Test for missing request

--- pact_elixir/test/dsl_test.exs
+++ pact_elixir/test/dsl_test.exs
@@ -22,6 +22,24 @@ defmodule PactElixir.DslTest do
     {:ok}
   end
 
+  test "Mock server includes mismatch without request" do
+    provider =
+      service_provider(consumer: "PactTester", provider: "PactProvider")
+      |> add_interaction(
+        "give me foo",
+        given("foo exists"),
+        with_request(method: :get, path: "/foo"),
+        will_respond_with(status: 200, body: "bar")
+      )
+      |> build
+
+    assert mock_server_matched?(provider) == false
+    [failure | _tail] = mock_server_mismatches(provider) |> Poison.decode!
+    assert %{"type" => "missing-request"} = failure
+
+    {:ok}
+  end
+
   defp get_request(provider, path) do
     %HTTPoison.Response{} = HTTPoison.get!("http://localhost:#{provider.port}#{path}")
   end
