e11bff69508f011235a6cdbacc72d07ebe109807
Ronald Holshausen <uglyog@gmail.com>
2017-10-20 10:33:44 +1100

    Correct the paths in the release script after changing to cargo workspace

--- rust/pact_mock_server/release-linux.sh
+++ rust/pact_mock_server/release-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 cargo clean
 cargo build --release
-gzip -c target/release/libpact_mock_server.so > target/release/libpact_mock_server-linux-x86_64-$1.so.gz
-gzip -c target/release/libpact_mock_server.a > target/release/libpact_mock_server-linux-x86_64-$1.a.gz
+gzip -c ../target/release/libpact_mock_server.so > ../target/release/libpact_mock_server-linux-x86_64-$1.so.gz
+gzip -c ../target/release/libpact_mock_server.a > ../target/release/libpact_mock_server-linux-x86_64-$1.a.gz
