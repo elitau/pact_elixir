e902293074096b1a488891d7c2c3f619c627ddde
Eduard Litau <eduard.litau@gmail.com>
2017-10-25 17:22:43 +0200

    Convert errors to elixir while writing pact file

--- pact_elixir/lib/pact_elixir/pact_mock_server.ex
+++ pact_elixir/lib/pact_elixir/pact_mock_server.ex
@@ -27,6 +27,7 @@ defmodule PactElixir.PactMockServer do
   def create_mock_server(_pact_json, _port), do: throw(:nif_not_loaded)
 
   def mismatches(%ServiceProvider{} = provider) do
+    # TODO: fails with seg fault when called with not used port
     {:ok, mismatches} = mock_server_mismatches(provider.port)
     mismatches
   end
@@ -41,27 +42,25 @@ defmodule PactElixir.PactMockServer do
   def mock_server_matched(_port), do: throw(:nif_not_loaded)
 
   def write_pact_file(%ServiceProvider{} = provider) do
-    result = if matched?(provider) do
+    provider
+    |> write_pact_file_with_errors
+  end
+
+  def write_pact_file_with_errors(%ServiceProvider{} = provider) do
+    if matched?(provider) do
       write_pact_file(provider.port, provider.pact_output_dir_path)
       |> process_write_pact_file_error
     else
       # Do not write file when mismatches happend
-      {:mismatches_happend, false}
+      {:error, :mismatches_prohibited_file_output}
     end
-
-    result
   end
 
   # Successfully written
-  defp process_write_pact_file_error({:ok, 0}), do: true
-  # TODO: raise error unless result == 0
-  # Errors are returned as positive values.
-  #
-  # | Error | Description |
-  # |-------|-------------|
-  # | 1 | A general panic was caught |
-  # | 2 | The pact file was not able to be written |
-  # | 3 | A mock server with the provided port was not found |
+  defp process_write_pact_file_error({:ok, 0}), do: {:success, true}
+  defp process_write_pact_file_error({:ok, 1}), do: {:error, :general_panic_caught}
+  defp process_write_pact_file_error({:ok, 2}), do: {:error, :pact_file_was_not_able_to_be_written}
+  defp process_write_pact_file_error({:ok, 3}), do: {:error, :mock_server_with_the_provided_port_was_not_found}
 
   def write_pact_file(_port, _dir_path), do: throw(:nif_not_loaded)
 end
--- pact_elixir/test/pact_mock_server_test.exs
+++ pact_elixir/test/pact_mock_server_test.exs
@@ -1,6 +1,6 @@
 defmodule PactElixir.PactMockServerTest do
   use ExUnit.Case
-  alias PactElixir.PactMockServer
+  alias PactElixir.{PactMockServer, ServiceProvider}
 
   @pact """
   {
@@ -27,9 +27,6 @@ defmodule PactElixir.PactMockServerTest do
         "metadata": {
           "pact-specification": {
             "version": "2.0.0"
-          },
-          "pact-jvm": {
-            "version": ""
           }
         }
       }
@@ -41,12 +38,12 @@ defmodule PactElixir.PactMockServerTest do
     assert get_request("/call_me").body == "Stop calling me"
   end
 
-  # test "do not write pact file when mismatches happend" do
+  # TODO
+  # test "do not write pact file when no server is available on given port" do
   #   dir_path = Path.join(File.cwd!, "test")
   #   exported_pact_file_path = Path.join(dir_path, "PactTester-PactProvider.json")
-  #   PactMockServer.create_mock_server(@pact, @port) == {:ok, @port}
 
-  #   assert {:ok, result} = PactMockServer.write_pact_file(@port, dir_path)
+  #   assert {:error, :mismatches_prohibited_file_output} = PactMockServer.write_pact_file(%ServiceProvider{pact_output_dir_path: dir_path, port: 50456})
 
   #   refute File.exists?(exported_pact_file_path)
   # end
