c3c218d24625304c7f30f98ad97fc3d90cfdb60c
Eduard Litau <eduard.litau@gmail.com>
2018-08-26 16:53:58 +0200

    Downgraded ex_doc because 0.19 works only with elixir 1.7

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule PactElixir.MixProject do
     [
       {:rustler, "~> 0.10"},
       {:poison, "~> 3.1"},
-      {:ex_doc, "~> 0.18", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.18.0", only: :dev, runtime: false},
       {:httpoison, "~> 1.0", only: :test},
       {:excoveralls, "~> 0.7", only: :test},
       {:temp, "~> 0.4", only: :test},
