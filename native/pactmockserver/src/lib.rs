68b83b51315c922f8b3cea3e2b56a9a3354c80a7
Eduard Litau <eduard.litau@gmail.com>
2018-08-26 15:49:04 +0200

    use newest version of rustler and public repo of pactmockserver

--- native/pactmockserver/src/lib.rs
+++ native/pactmockserver/src/lib.rs
@@ -1,14 +1,14 @@
 #[macro_use] extern crate rustler;
+#[macro_use] extern crate lazy_static;
 
 // Disabled for now, but still
 // wondering why rustler generated this in the first place
-// #[macro_use] extern crate rustler_codegen; 
+// #[macro_use] extern crate rustler_codegen;
 
-#[macro_use] extern crate lazy_static;
+use rustler::{Env, Term, NifResult, Encoder};
 extern crate pact_mock_server;
 extern crate libc;
 
-use rustler::{NifEnv, NifTerm, NifResult, NifEncoder};
 use pact_mock_server::create_mock_server;
 use pact_mock_server::MockServerError;
 use pact_mock_server::mock_server_mismatches;
@@ -41,28 +41,28 @@ rustler_export_nifs! {
     None
 }
 
-fn create_mock_server_call<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
+fn create_mock_server_call<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     let pact_json: &str = try!(args[0].decode());
     let port_arg: i32 = try!(args[1].decode());
 
     match create_mock_server(pact_json, port_arg) {
-        Ok(port) => 
+        Ok(port) =>
             Ok((atoms::ok(), port).encode(env)),
-        Err(MockServerError::MockServerFailedToStart) => 
+        Err(MockServerError::MockServerFailedToStart) =>
             Ok((atoms::error(), atoms::mock_server_failed_to_start()).encode(env)),
-        Err(MockServerError::InvalidPactJson) => 
+        Err(MockServerError::InvalidPactJson) =>
             Ok((atoms::error(), atoms::invalid_pact_json()).encode(env))
     }
 }
 
-fn mock_server_mismatches_call<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
+fn mock_server_mismatches_call<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     let port: i32 = try!(args[0].decode());
-    // TODO: This works only because mock_server_mismatches returns a string now: 
+    // TODO: This works only because mock_server_mismatches returns a string now:
     // Calling to_string() on json!(mismatches) in pact-reference/rust/pact_mock_server/src/lib.rs:657
     Ok((atoms::ok(), mock_server_mismatches(port)).encode(env))
 }
 
-fn mock_server_matched_call<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
+fn mock_server_matched_call<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     let port: i32 = try!(args[0].decode());
 
     let matched: bool = mock_server_matched(port);
@@ -70,21 +70,21 @@ fn mock_server_matched_call<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifRes
     Ok((atoms::ok(), matched).encode(env))
 }
 
-fn write_pact_file_call<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
+fn write_pact_file_call<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     let port: i32 = try!(args[0].decode());
     let dir_path: String = try!(args[1].decode());
 
     match write_pact_file(port, Some(dir_path)) {
-        Ok(_result) => 
+        Ok(_result) =>
             Ok((atoms::ok()).encode(env)),
-        Err(WritePactFileErr::IOError) => 
+        Err(WritePactFileErr::IOError) =>
             Ok((atoms::error(), atoms::io_error()).encode(env)),
-        Err(WritePactFileErr::NoMockServer) => 
+        Err(WritePactFileErr::NoMockServer) =>
             Ok((atoms::error(), atoms::no_mock_server_running_on_port()).encode(env))
     }
 }
 
-fn cleanup_mock_server_call<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
+fn cleanup_mock_server_call<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     let port: i32 = try!(args[0].decode());
 
     Ok((atoms::ok(), cleanup_mock_server_ffi(port)).encode(env))
