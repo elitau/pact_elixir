252a0df453dc70a18c7246fb5efec817746abd28
Eduard Litau <eduard.litau@gmail.com>
2019-05-16 13:38:53 +0200

    Jaxon can generate JSON path

--- mix.exs
+++ mix.exs
@@ -41,6 +41,7 @@ defmodule PactElixir.MixProject do
     [
       {:rustler, "~> 0.18"},
       {:poison, "~> 4.0"},
+      {:jaxon, "~> 1.0"},
       {:ex_doc, "~> 0.18.0", only: :dev, runtime: false},
       {:httpoison, "~> 1.0", only: :test},
       {:excoveralls, "~> 0.7", only: :test},
