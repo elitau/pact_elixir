a87e179cf10aea82bc0a2f3d086d81888615f5eb
Eduard Litau <eduard.litau@gmail.com>
2019-06-22 00:32:03 +0200

    Stop using deprecated function

--- test/dsl_integration_test.exs
+++ test/dsl_integration_test.exs
@@ -27,7 +27,7 @@ defmodule PactElixir.DslIntegrationTest do
     assert "\"bar\"" == get_request(provider, "/foo").body
 
     assert [] == mock_server_mismatches(provider)
-    assert true == mock_server_matched?(provider)
+    assert true == PactMockServer.matched?(provider)
   end
 
   test "has a host url", %{provider: provider} do
@@ -45,7 +45,7 @@ defmodule PactElixir.DslIntegrationTest do
   end
 
   test "Mock server includes mismatch without mocked request being made", %{provider: provider} do
-    assert mock_server_matched?(provider) == false
+    assert PactMockServer.matched?(provider) == false
 
     [failure | _tail] = mock_server_mismatches(provider)
 
