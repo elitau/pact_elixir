384c4d0b83bb140f0e719ee8d6a9afeae8aead7f
Eduard Litau <eduard.litau@gmail.com>
2020-01-12 18:38:17 +0100

    Keep mix.exs file in sync with mix.lock file

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule PactElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.18"},
+      {:rustler, "~> 0.21"},
       {:poison, "~> 4.0"},
       {:ex_doc, "~> 0.18.0", only: :dev, runtime: false},
       {:httpoison, "~> 1.0", only: :test},
