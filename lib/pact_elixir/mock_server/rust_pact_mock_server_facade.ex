9ef0c496e9e85aee95f5d70b1a908489fe2dbfa3
Eduard Litau <eduard.litau@gmail.com>
2021-11-28 00:26:25 +0100

    Use precompiled NIF if present

--- lib/pact_elixir/mock_server/rust_pact_mock_server_facade.ex
+++ lib/pact_elixir/mock_server/rust_pact_mock_server_facade.ex
@@ -7,7 +7,41 @@ defmodule PactElixir.RustPactMockServerFacade do
   This file is excluded from the coverage tool.
   """
 
-  use Rustler, otp_app: :pact_elixir, crate: "pactmockserver"
+  require Logger
+
+  mix_config = Mix.Project.config()
+  version = mix_config[:version]
+  github_url = mix_config[:package][:links]["GitHub"]
+
+  rustler_opts = [otp_app: :pact_elixir, crate: "pactmockserver", mode: :release]
+  env_config = Application.get_env(rustler_opts[:otp_app], PactElixir, [])
+
+  opts =
+    if System.get_env("PACTELIXIR_BUILD") in ["1", "true"] or env_config[:build_from_source] do
+      rustler_opts
+    else
+      case PactElixir.RustPactMockServerFacade.Precompiled.download_or_reuse_nif_file(
+             rustler_opts,
+             base_url: "#{github_url}/releases/download/v#{version}",
+             version: version
+           ) do
+        {:ok, new_opts} ->
+          new_opts
+
+        {:error, error} ->
+          error =
+            "Error while downloading precompiled NIF: #{error}\n\nSet HTML5EVER_BUILD=1 env var to compile the NIF from scratch. You can also configure this application to force compilation:\n\n    config :html5ever, Html5ever, build_from_source: true\n"
+
+          if Mix.env() == :prod do
+            raise error
+          else
+            Logger.debug(error)
+            rustler_opts
+          end
+      end
+    end
+
+  use Rustler, opts
 
   def create_mock_server(_pact_json, _port), do: throw(:nif_not_loaded)
   def mock_server_mismatches(_port), do: throw(:nif_not_loaded)
