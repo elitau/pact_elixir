b4fe0900e31d631583e4bd1f42e8414e58d83872
Eduard Litau <eduard.litau@gmail.com>
2021-11-28 00:47:13 +0100

    Remove warnings created during precompilation setup

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule PactElixir.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :inets, :public_key],
       mod: {PactElixir.Application, []}
     ]
   end
