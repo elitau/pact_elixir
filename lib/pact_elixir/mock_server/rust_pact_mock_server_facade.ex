a3b4bb958ea9ac38283031ada12accd9bded4673
Eduard Litau <eduard.litau@gmail.com>
2022-02-02 21:57:08 +0100

    DRYup

--- lib/pact_elixir/mock_server/rust_pact_mock_server_facade.ex
+++ lib/pact_elixir/mock_server/rust_pact_mock_server_facade.ex
@@ -43,11 +43,13 @@ defmodule PactElixir.RustPactMockServerFacade do
 
   use Rustler, opts
 
-  def create_mock_server(_pact_json, _port), do: throw(:nif_not_loaded)
-  def mock_server_mismatches(_port), do: throw(:nif_not_loaded)
+  def create_mock_server(_pact_json, _port), do: err()
+  def mock_server_mismatches(_port), do: err()
 
   # @spec mock_server_matched(number) :: {:ok, boolean}
-  def mock_server_matched(_port), do: throw(:nif_not_loaded)
-  def write_pact_file(_port, _dir_path), do: throw(:nif_not_loaded)
-  def cleanup_mock_server(_port), do: throw(:nif_not_loaded)
+  def mock_server_matched(_port), do: err()
+  def write_pact_file(_port, _dir_path), do: err()
+  def cleanup_mock_server(_port), do: err()
+
+  defp err, do: :erlang.nif_error(:nif_not_loaded)
 end
